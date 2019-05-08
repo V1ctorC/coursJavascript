e5db87c2758734c1a69119f979dcbb2bea03a538
Victor <victor.du.77@hotmail.fr>
2019-05-08 12:08:58 +0200

    Course 23 - Handling JSON errors

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -1,45 +1,41 @@
-
+'use strict';
 (function(window, $) {
-
-    'use strict';
-
     window.RepLogApp = function ($wrapper) {
         this.$wrapper = $wrapper;
         this.helper = new Helper(this.$wrapper);
-
         this.$wrapper.on(
             'click',
             '.js-delete-rep-log',
             this.handleRepLogDelete.bind(this)
         );
-
         this.$wrapper.on(
             'click',
             'tbody tr',
             this.handleRowClick.bind(this)
         );
-
         this.$wrapper.on(
             'submit',
-            '.js-new-rep-log-form',
+            this._selectors.newRepForm,
             this.handleNewFormSubmit.bind(this)
-        )
-
-
+        );
     };
-
     $.extend(window.RepLogApp.prototype, {
+        _selectors: {
+            newRepForm: '.js-new-rep-log-form'
+        },
+        updateTotalWeightLifted: function () {
+            this.$wrapper.find('.js-total-weight').html(
+                this.helper.calculateTotalWeight()
+            );
+        },
         handleRepLogDelete: function (e) {
             e.preventDefault();
-
             var $link = $(e.currentTarget);
-
             $link.addClass('text-danger');
             $link.find('.fa')
                 .removeClass('fa-trash')
                 .addClass('fa-spinner')
-                .addClass('fa-spin')
-
+                .addClass('fa-spin');
             var deleteUrl = $link.data('url');
             var $row = $link.closest('tr');
             var self = this;
@@ -48,67 +44,69 @@
                 method: 'DELETE',
                 success: function () {
                     $row.fadeOut('normal', function () {
-                        //same as $(this).remove();
-                        $row.remove();
+                        $(this).remove();
                         self.updateTotalWeightLifted();
                     });
                 }
-            })
+            });
         },
-
         handleRowClick: function () {
-            console.log('row clicked !');
+            console.log('row clicked!');
         },
-
-        updateTotalWeightLifted: function () {
-
-            this.$wrapper.find('.js-total-weight').html(
-                this.helper.calculateTotalWeight()
-            );
-        },
-
-        handleNewFormSubmit: function (e) {
+        handleNewFormSubmit: function(e) {
             e.preventDefault();
-
             var $form = $(e.currentTarget);
             var formData = {};
             $.each($form.serializeArray(), function(key, fieldData) {
-                formData[fieldData.name] = fieldData.value;
+                formData[fieldData.name] = fieldData.value
             });
+            var self = this;
             $.ajax({
-                url: $form.data('url '),
-                method: 'post',
+                url: $form.data('url'),
+                method: 'POST',
                 data: JSON.stringify(formData),
-                success: function (data) {
+                success: function(data) {
                     // todo
-                    console.log('Success !');
+                    console.log('success!');
                 },
-                error: function (jqXHR) {
-                    // todo
-                    console.log('Error :(');
+                error: function(jqXHR) {
+                    var errorData = JSON.parse(jqXHR.responseText);
+                    self._mapErrorsToForm(errorData.errors);
                 }
-            })
-
+            });
+        },
+        _mapErrorsToForm: function(errorData) {
+            // reset things!
+            var $form = this.$wrapper.find(this._selectors.newRepForm);
+            $form.find('.js-field-error').remove();
+            $form.find('.form-group').removeClass('has-error');
+            $form.find(':input').each(function() {
+                var fieldName = $(this).attr('name');
+                var $wrapper = $(this).closest('.form-group');
+                if (!errorData[fieldName]) {
+                    // no error!
+                    return;
+                }
+                var $error = $('<span class="js-field-error help-block"></span>');
+                $error.html(errorData[fieldName]);
+                $wrapper.append($error);
+                $wrapper.addClass('has-error');
+            });
         }
     });
-
     /**
-     *  A "private" object
+     * A "private" object
      */
     var Helper = function ($wrapper) {
         this.$wrapper = $wrapper;
     };
-
     $.extend(Helper.prototype, {
-        calculateTotalWeight: function () {
+        calculateTotalWeight: function() {
             var totalWeight = 0;
             this.$wrapper.find('tbody tr').each(function () {
                 totalWeight += $(this).data('weight');
             });
-
             return totalWeight;
         }
-    })
-
-
+    });
 })(window, jQuery);
